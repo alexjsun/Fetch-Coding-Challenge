3c72cee8502a287706fc39671733c51b98f7bdac
sunaj <sunaj@uchicago.edu>
2024-09-17 18:50:34 -0700

    initial project setup

---
+++ Fetch-Coding-Challenge/Fetch-Coding-Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch-Coding-Challenge
+//
+//  Created by Alex Sun on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch-Coding-Challenge/Fetch-Coding-Challenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallengeApp
+//
+//  Created by Alex Sun on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
